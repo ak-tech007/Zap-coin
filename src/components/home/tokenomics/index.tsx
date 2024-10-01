002752a2d5cc921ec8544a55306138ba8994f7cc
ak-tech007 <lakatanana1130@gmail.com>
2024-10-01 13:06:57 -0400

    tree

--- src/components/home/tokenomics/index.tsx
+++ src/components/home/tokenomics/index.tsx
@@ -3,8 +3,8 @@ import { Content } from "./content";
 export const Tokenomics = () => {
   return (
     <>
-      <div id="Tokenomics" className=" relative ">
-        <img src="/bg/tokenomics.jpg" className="w-full " />       
+      <div id="Tokenomics" className=" relative mt-[-8vw] z-20 ">
+        <img src="/bg/tokenomics.jpg" className="w-full " />   
         <Content />
       </div>
     </>
